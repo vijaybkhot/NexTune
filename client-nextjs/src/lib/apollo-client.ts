451daf2d7168edfe6aa3995769c255891bdfebd4
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-30 13:13:39 -0400

    feat: dockerize project for local development
    - Added Dockerfiles for frontend and backend
    - Added docker-compose.yml to orchestrate services
    - Configured environment variables for local Docker use

--- client-nextjs/src/lib/apollo-client.ts
+++ client-nextjs/src/lib/apollo-client.ts
@@ -1,7 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
+<<<<<<< Updated upstream
   uri: process.env.NEXT_PUBLIC_GRAPHQL_API || "http://localhost:4000/graphql",
+=======
+  uri: "/graphql",
+>>>>>>> Stashed changes
   cache: new InMemoryCache(),
 });
 
